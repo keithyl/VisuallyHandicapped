96f8dece224054ed128daf02806f5e87630396a7
Qi Yang <qiyang.tan.2012@sis.smu.edu.sg>
2016-03-12 20:06:38 +0800

    edited places.js to add in functions to track boarding and alighting

--- web/js/places.js
+++ web/js/places.js
@@ -6,6 +6,9 @@ var infowindow;
 var geocoder;
 var markersArray = new Array();
 var gmarkers = [];
+var hasBoarded = false;
+var shakeDetected = false;
+var serviceNo;
 
 function initialize() {
     geocoder = new google.maps.Geocoder();
@@ -14,68 +17,97 @@ function initialize() {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         panControl: false,
         streetViewControl: false,
-        mapTypeControl: false,
+        mapTypeControl: false
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    //HTML5 geolocation
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(function(position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            var latlng = new google.maps.LatLng(lat, lng);
-            var myLocation = new google.maps.LatLng(lat, lng);
-            //document.getElementById("grid").value= myLocation.lat() + "," + myLocation.lng();
-            geocoder.geocode({'latLng': myLocation}, function(results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    if (results[1]) {
-                        //clearMarkers();
-                        removeMarkers();
-                        map.setZoom(18);
-                        marker = new google.maps.Marker({
-                            position: myLocation,
-                            draggable: false,
-                            //animation: google.maps.Animation.DROP,
-                            map: map
-                        });
-                        gmarkers.push(marker);
-                        //contentString = '<div id="iwContent">Lat: <span id="latbox">' + myLocation.lat() + '</span><br />Lng: <span id="lngbox">' + myLocation.lng() + '</span><br/><span id="addrbox">' + results[1].formatted_address + '</span></div>';
-                        //window.alert(results[1].formatted_address);
-                        //infoWindow.setContent(contentString);
-                        //infoWindow.open(map,marker);
-                    } else {
-                        alert('No results found');
+    callGeolocation();
+    /*if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(function(position) {
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                var myLocation = new google.maps.LatLng(lat, lng);
+                geocoder.geocode({'latLng': myLocation}, function(results, status) {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        if (results[1]) {
+                            removeMarkers();
+                            map.setZoom(18);
+                            marker = new google.maps.Marker({
+                                position: myLocation,
+                                draggable: false,
+                                //animation: google.maps.Animation.DROP,
+                                map: map
+                            });
+                            gmarkers.push(marker);
+                        } else {
+                            alert('No results found');
+                        }
                     }
-
-                }
-            });
-            pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            /*
-             infowindow = new google.maps.InfoWindow({
-             map: map,
-             position: pos,
-             content: 'You Are Here'
-             });*/
-
-            var request = {
-                location: pos,
-                radius: 100,
-                types: ['bus_station']
-            };
-
-            map.setCenter(pos);
-
-            infowindow = new google.maps.InfoWindow();
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch(request, callback);
-
-        },
-                function() {
-                    handleNoGeolocation(true);
                 });
-    } else {
-        handleNoGeolocation(false);
+                pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+                var request = {
+                    location: pos,
+                    radius: 150,
+                    types: ['bus_station']
+                };
+
+                map.setCenter(pos);
+
+                infowindow = new google.maps.InfoWindow();
+                var service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request, callback);
+            }, function() {
+                handleNoGeolocation(true);
+            });
+        } else {
+            handleNoGeolocation(false);
+        } */
+    
+    //HTML5 geolocation - throw into function
+    function callGeolocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(function(position) {
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                var myLocation = new google.maps.LatLng(lat, lng);
+                geocoder.geocode({'latLng': myLocation}, function(results, status) {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                        if (results[1]) {
+                            removeMarkers();
+                            map.setZoom(18);
+                            marker = new google.maps.Marker({
+                                position: myLocation,
+                                draggable: false,
+                                //animation: google.maps.Animation.DROP,
+                                map: map
+                            });
+                            gmarkers.push(marker);
+                        } else {
+                            alert('No results found');
+                        }
+                    }
+                });
+                pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+                var request = {
+                    location: pos,
+                    radius: 150,
+                    types: ['bus_station']
+                };
+
+                map.setCenter(pos);
+
+                infowindow = new google.maps.InfoWindow();
+                var service = new google.maps.places.PlacesService(map);
+                service.nearbySearch(request, callback);
+            }, function() {
+                handleNoGeolocation(true);
+            });
+        } else {
+            handleNoGeolocation(false);
+        }
     }
 
     function callback(results, status) {
@@ -93,13 +125,11 @@ function initialize() {
     }
 
     function createMarker(place) {
-        //deleteMarkers();
-        //var busstopID;
-        //var placeLoc = place.geometry.location;
-        //console.log("Place location: " + place.place_id);
-        //console.log(place.name);
-        
-        getBusArrival(place.place_id);
+        if (! hasBoarded) {
+            // hard code to SMU SOA bus stop for testing
+            getBusArrival('ChIJeT0RdqMZ2jERRobPHBNqyPM');
+            //getBusArrival(place.place_id);
+        }
 
         var image = 'images/nearestbuses.png';
         var marker = new google.maps.Marker({
@@ -112,7 +142,6 @@ function initialize() {
             infowindow.setContent(place.name);
             infowindow.open(map, this);
         });
-
     }
 
     function deleteMarkers() {
@@ -127,22 +156,86 @@ function initialize() {
     function getBusArrival(placeID) {
         var url= "https://busservices.firebaseio.com/stopsPlaceId/" + placeID + ".json";
         
+        console.log(url);
         $.getJSON(url, function(data) {
             $('#busStopName').html(data.name + ' (' + data.stopId + ')');
             var stopID = data.stopId;            
-            var serviceNo = $('#serviceNo').html();
+            serviceNo = $('#serviceNo').html();
             var busTimingUrl = "https://intelbus.herokuapp.com/?id=" + stopID + "&serviceNo=" + serviceNo;
+            console.log(busTimingUrl);
             $.getJSON(busTimingUrl, function(busData) {
                 if (busData[0].nextBus.duration === 'Arriving') {
                     $('#Duration1').html(busData[0].nextBus.duration);
                 } else {
                     $('#Duration1').html(busData[0].nextBus.duration + ' Mins');
                 }
-                $('#Duration2').html(busData[0].subsequentBus.duration + ' Mins');
+                
+                if (busData[0].subsequentBus.duration === 'Arriving') {
+                    $('#Duration2').html(busData[0].subsequentBus.duration);
+                } else {
+                    $('#Duration2').html(busData[0].subsequentBus.duration + ' Mins');
+                }
                 //$('#Duration3').html(busData[0].subsequentBus3.duration);
             });
         });
     }
 
+    // BOARDED BUS double click event
+    $('body').dblclick(function() {
+        //window.alert('aaa');
+        
+        hasBoarded = true;
+       // callGeolocation();
+       getBusStopInfo();
+
+    });
+    
+    function getBusStopInfo() {
+        //remove table with bus arrival info
+        $('#busTiming').remove();
+        
+        //get next bus stop name from firebase
+               /* $.getJSON('', function(data) {
+            
+        });*/
+        
+        //add table containing next bus stop name
+        if (! $('#busStopName').length) {
+            $('.table-responsive').append('<table class="table table-hover" id="busStopName">\n\
+                                            <tr><th width=15%>' + "Bus Onboard: " + '</th><td>' + serviceNo + '</td></tr>\n\
+                                            <tr id="nextBusStop"><th>' + "Next Bus Stop: " + '</th><td>SMU</td></tr></table>');
+        }
+    }
+    
+    // tap once to play bus stop name after onboard the bus
+    $('body').click(function() {
+        if (hasBoarded && !shakeDetected) {
+            var text = $('#nextBusStop').text();
+            responsiveVoice.speak(text);
+            
+            // for chinese
+            //responsiveVoice.speak(text, "Chinese Female");
+        }
+    });
+    
+    // eventlistener for shake event to indicate alighting
+    var shakeEvent = new Shake({threshold: 15});
+    shakeEvent.start();
+    window.addEventListener('shake', function(){
+        shakeDetected = true;
+        if (hasBoarded && shakeDetected) {
+            alert('SHAKE DETECTED ALIGHTED');
+            
+            // play message - confirm alight, tap once for no, tap twice for yes
+            $('body').dblclick(function() {
+               //alert('CONFIRM ALIGHTED');
+
+               responsiveVoice.speak('You have alighted from the bus');
+               
+               //redirect to main page
+               window.location.replace(window.location.href);
+            });
+        }
+    }, false);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
